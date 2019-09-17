eab48ede387118479afbd0008ccd17e063e94b9a
Barselaar <kevin.barselaar@capgemini.com>
2019-09-17 11:13:32 +0200

    fix

--- src/com/capgemini/se_track/Calculator.java
+++ src/com/capgemini/se_track/Calculator.java
@@ -5,11 +5,11 @@ public class Calculator {
     public Calculator() { }
 
     public int add(int first, int second) {
-        return first + second;
+        return first - -second;
     }
 
     public int subtract(int first, int second) {
-        return first - second;
+        return first + -second;
     }
 
     public int multiple(int number, int times) {
