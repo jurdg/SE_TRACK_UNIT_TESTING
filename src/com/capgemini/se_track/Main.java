1171b0b0ee0e9557616bed4962b25fe207d1c4d5
Ingen <claire.van.ingen@capgemini.com>
2019-09-18 11:12:32 +0200

    claire exceptionhandling

--- src/com/capgemini/se_track/Main.java
+++ src/com/capgemini/se_track/Main.java
@@ -25,46 +25,76 @@ public class Main {
         String b = "b";
         String c = "c";
         int i = 10;
-
-        String result = a + b;
-        result += c;
-
+//EXCEPTION
+		try{
+			String result = a + b;
+        }catch (NullPointerException e ){
+			System.out.println("Cannot add nul");	
+		}
+			
+		result += c;
+		
         System.out.println(result + Integer.toString(i));
     }
 
     private static void getCharacter(int index) {
         String a = "This is a test string ";
-        char c = a.charAt(24);
+//EXCEPTION		
+		try{
+			char c = a.charAt(24);
+		}catch IndexOutOfBoundsException e{
+			System.out.println("No character at this index");
+		}
+			
+		
         System.out.println(c);
     }
-
+//EXCEPTION
     private static void division() {
         int a, b, result;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Input two integers");
+		
+		try{
+			a = input.nextInt();
+			
+		}catch (Exception e){
+			throw new Exception("Enter a valid entry");
+		}
 
-        a = input.nextInt();
-        b = input.nextInt();
-
+		try{
+			b = input.nextInt();
+			
+		}catch (Exception e){
+			throw new Exception("Enter a valid entry");
+		}
         result = a / b;
 
         System.out.println("Result = " + result);
     }
 
     private static void formatInteger() {
-        String str = "1;";
+ //EXCEPTION
+		String str = "1;";
+		try{
         int num = Integer.parseInt (str) ;
+		}catch (Error e){
+			System.out.prinln("Cannot convert to int");
+		}
 
         System.out.println(num);
     }
 
     private static String languages[] = { "C", "C++", "Java", "Perl", "Python" };
     private static void loopOverArray() {
-
+//EXCEPTION
         for (int c = 1; c <= 5; c++) {
+			try{
             System.out.println(languages[c]);
-        }
+        } catch ArrayIndexOutOfBoundsException e {
+			System.out.println("Array out of bounds");
+		}
     }
 
     private static void waitForInput() {
